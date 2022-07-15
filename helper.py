28902a7333d12bc0a423109b9161c40bb82ac9f7
svshiva <shivam2001vishwakarma@gmail.com>
2022-07-15 19:30:10 +0530

    API for IMG URL

--- helper.py
+++ helper.py
@@ -6,7 +6,7 @@ from urllib.request import urlopen
 def img64_to_cvImg(img64):
     img64=img64.replace('data:image/jpeg;base64,','')
     img=base64.b64decode(img64)
-    img=numpy.fromstring(img,numpy.uint8)
+    img=np.fromstring(img,np.uint8)
     img=cv2.imdecode(img,cv2.IMREAD_COLOR)
     return img
 
